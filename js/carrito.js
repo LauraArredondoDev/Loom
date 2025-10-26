e4e1a4ccf9fd292e7afe765f15f8f3dcd571d1cd
LauraArredondoDev <lauraarredondodev@gmail.com>
2025-10-26 13:22:35 +0100

    borrar líneas vacías

--- js/carrito.js
+++ js/carrito.js
@@ -1,6 +1,3 @@
-//Paso 1: obtener la barra lateral y guardarla en una constante
-//Paso 2: hacer un if que compruebe si la barra lateral tiene la clase .nav-lat-oculta
-
 function mostrarCarrito() {
     const carrito = document.getElementById('carrito');
 
