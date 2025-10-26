54b4b243554dda035639c006cf46157a7a711c49
LauraArredondoDev <lauraarredondodev@gmail.com>
2025-10-26 15:11:07 +0100

    función para añadir el contenido al carrito

--- js/carrito.js
+++ js/carrito.js
@@ -9,3 +9,40 @@ function ocultarCarrito() {
 
     carrito.classList.replace('carrito-visible', 'carrito-oculto');
 }
+
+function añadirContenidoCarrito(nombre, precio, imgUrl) {
+
+    const divNuevo = document.createElement('div');
+    divNuevo.classList.add('carrito-item');
+
+    const img = document.createElement('img');
+    img.src = imgUrl;
+    divNuevo.appendChild(img);
+
+    const spanPrenda = document.createElement('span');
+    spanPrenda.classList.add('pan-prenda');
+    spanPrenda.innerText = nombre || '';
+    divNuevo.appendChild(spanPrenda);
+
+    const spanPrecio = document.createElement('span');
+    spanPrecio.classList.add('span-precio');
+    spanPrecio.innerText = precio || '';
+    divNuevo.appendChild(spanPrecio);
+
+    const iconoBasura = document.createElement('i');
+    iconoBasura.classList.add('fa-solid', 'fa-trash');
+    divNuevo.appendChild(iconoBasura);
+
+    const cuerpoCarrito = document.getElementById('cuerpo-carrito');
+
+    cuerpoCarrito.appendChild(divNuevo);
+
+}
+
+/**
+ * <img src="img/Vestido-gris.png">
+<span>Vestido de lana gris</span>
+<span>25,99€</span>
+<i class="fa-solid fa-trash"></i>
+ */
+
