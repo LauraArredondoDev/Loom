9083157674fecd0d4ce81f74729f76c09d9a075d
LauraArredondoDev <lauraarredondodev@gmail.com>
2025-10-29 17:35:02 +0100

    Añadido en el carrito botón de comprar y que cuando no haya prendas de ropa desaparezca el boton de comprar

--- js/carrito.js
+++ js/carrito.js
@@ -33,12 +33,37 @@ function añadirContenidoCarrito(nombre, precio, imgUrl) {
     iconoBasura.classList.add('fa-solid', 'fa-trash');
     divNuevo.appendChild(iconoBasura);
 
+    iconoBasura.addEventListener('click', () => {
+        divNuevo.remove();
+
+        //Verifico si quedan items(prendas) en el carrito
+        const item = cuerpoCarrito.querySelectorAll('.carrito-item');
+        const btnComprar = cuerpoCarrito.querySelector('.btn-comprar');
+
+        //Si no quedan prendas en el carrito y existe el botón de comprar lo elimino.
+        if(item.length === 0 && btnComprar) {
+            btnComprar.remove();
+        }
+    });
+
     const cuerpoCarrito = document.getElementById('cuerpo-carrito');
     cuerpoCarrito.appendChild(divNuevo);
 
-    iconoBasura.addEventListener('click', () => {
-        divNuevo.remove();
-    });
+    // Se comprueba si existe el botón de comprar y si no existe se crea.
+    if(!cuerpoCarrito.querySelector('.btn-comprar')) {
+        const btnComprar = document.createElement('button');
+        btnComprar.classList.add('btn-comprar');
+        btnComprar.innerHTML = 'Comprar';
+        cuerpoCarrito.appendChild(btnComprar);
+    }
+
+    // Aquí se busca el botón ya existente y se mueve al final.
+    const btnComprarExistente = cuerpoCarrito.querySelector('.btn-comprar');
+    if(btnComprarExistente) {
+        cuerpoCarrito.appendChild(btnComprarExistente);
+    }
+
+    
 }
 
 
